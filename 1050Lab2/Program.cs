75e5e14f002614ea1c7d8fa134bea46e95f7fd88
Dawn Mitchell <S00027159@acad.tri-c.edu>
2019-12-11 20:55:57 -0500

    Add project files.

---
+++ 1050Lab2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1050Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter information for person 1: ");
+
+            Person person1 = new Person();
+            person1.AskUserForNameAndAge();
+            person1.CreateSpouseIfMarried();
+
+            Console.WriteLine("Enter information for person 2: ");
+
+            Person person2 = new Person();
+            person2.AskUserForNameAndAge();
+            person2.CreateSpouseIfMarried();
+
+            Person spouse1 = new Spouse();
+            spouse1.AskUserForNameAndAge();
+            spouse1.CreateSpouseIfMarried();
+
+            Person spouse2 = new Spouse();
+            spouse2.AskUserForNameAndAge();
+            spouse2.CreateSpouseIfMarried();
+
+            System.Console.WriteLine("Average Age: " + Person.AverageAge());
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ 1050Lab2/Spouse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1050Lab2
+{
+    class Spouse
+    {
+        public string Spouse;
+    }
+}
